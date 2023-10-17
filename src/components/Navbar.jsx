692dbed34072ff89eebf8de7458da0110a71b42e
pawelo472 <pawel.kluczkiewicz@wp.pl>
2023-10-17 13:26:23 +0200

    "commit1"

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import  {Button, Menu, Typography, Avatar} from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined,UserOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from "../images/logo.png";
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar src={icon} size="large"/>
+            <Typography.Title level={2} className='logo'>
+                <Link to="/">Crypto</Link>
+            </Typography.Title>
+        </div>
+        <Menu theme='light'>
+            <Menu.Item icon={<HomeOutlined/>}>
+                <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined/>}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<UserOutlined/>}>
+                <Link to="/about">About Us</Link>
+            </Menu.Item>
+        </Menu>
+    </div>
+  )
+}
+
+export default Navbar
