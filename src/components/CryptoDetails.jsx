1f07f7112477f0c718d32e46706a562755f0da77
pawelo472 <pawel.kluczkiewicz@wp.pl>
2023-11-08 13:54:20 +0100

    Login form, cryptodetails and easy buy form

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Card,Row, Col, Statistic, Button, Space, Input } from 'antd';
+import { Typography, Card, Row, Col, Statistic, Button, Space, Input, Form } from 'antd';
 import { useGetCryptosDetailsQuery, useGetCryptosQuery } from '../services/CryptoApi';
 import '../App.css';
-import reactRouterDom,{Link, useParams} from 'react-router-dom';
+import millify from 'millify';
+import reactRouterDom, { Link, useParams } from 'react-router-dom';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
 const { Title, Text } = Typography;
+
 const CryptoDetails = () => {
   const { symbol } = useParams(); // Pobierz symbol z parametrów URL
 
@@ -48,27 +50,73 @@ const CryptoDetails = () => {
   }
 
   // Filtrujemy tylko symbole zawierające przekazany symbol
+  const filteredDetails = detailsData.filter(cryptodetails => cryptodetails.symbol === symbol);
   const filteredCryptos = cryptosData.filter(crypto => crypto.symbol === symbol);
   const price = filteredCryptos.length > 0 ? filteredCryptos[0].price : 0;
-  //const filteredDetails = detailsData.filter(cryptodetails => cryptodetails.symbol.toLowerCase() === symbol);
-  const stats=[
-    {title: 'Price in $', value: parseFloat(price).toFixed(4), icon: <DollarCircleOutlined/>},
-    //{title: '24h Volume', value: parseFloat(cryptodetails.volume).toFixed(4), icon: <DollarCircleOutlined/>},
-  ]
+  const volume24h = filteredDetails.length > 0 ? filteredDetails[0].volume : 0;
+  const openPrice = filteredDetails.length > 0 ? filteredDetails[0].openPrice : 0;
+  const highPrice = filteredDetails.length > 0 ? filteredDetails[0].highPrice : 0;
+  const lowPrice = filteredDetails.length > 0 ? filteredDetails[0].lowPrice : 0;
+  const askPrice = filteredDetails.length > 0 ? filteredDetails[0].askPrice : 0;
+  const bidPrice = filteredDetails.length > 0 ? filteredDetails[0].bidPrice : 0;
+  const count = filteredDetails.length > 0 ? filteredDetails[0].count : 0;
+
+  const stats = [
+    { title: 'Price: ', value: parseFloat(price).toFixed(4), icon: <DollarCircleOutlined /> },
+    { title: '24h Volume: ', value: parseFloat(volume24h).toFixed(4), icon: <DollarCircleOutlined /> },
+    { title: 'BidPrice: ', value: parseFloat(bidPrice).toFixed(4), icon: <DollarCircleOutlined /> },
+    { title: 'AskPrice: ', value: parseFloat(askPrice).toFixed(4), icon: <DollarCircleOutlined /> },
+    { title: 'Open Price in the session: ', value: parseFloat(openPrice).toFixed(4), icon: <DollarCircleOutlined /> },
+    { title: 'High Price in the session: ', value: parseFloat(highPrice).toFixed(4), icon: <DollarCircleOutlined /> },
+    { title: 'Low Price in the session: ', value: parseFloat(lowPrice).toFixed(4), icon: <DollarCircleOutlined /> },
+    { title: 'Number of transactions in the session: ', value: count, icon: <DollarCircleOutlined /> },
+  ];
+
+  // Define your form structure
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const onFinish = (values) => {
+    // Handle form submission here
+    console.log('Form values:', values);
+  };
+
   return (
     <div>
-      <Title level={2}>Crypto Details for {symbol.replace("USDT", "")}</Title>
-    {stats.map(({title, icon, value})=>(
-      <Col className="coin-stats">
-      <Col className="coin-stats-name">
-        <Text>{icon}</Text>
-        <Text>{title}</Text>
-      </Col>
-      <Text className="stats">{value}</Text>
-    </Col>
-    ))}
+      <Row gutter={16}>
+        <Col span={12}>
+          <Title level={2}>Crypto Details for {symbol}</Title>
+          {stats.map(({ title, icon, value }) => (
+            <Col className="stats-container">
+              <Col className="coin-value-statistics">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+                <Text className="stats">{value}</Text>
+              </Col>
+            </Col>
+          ))}
+        </Col>
+        <Col span={12}>
+        <Card title={`Buy Cryptocurrency (${symbol})`}>
+            <Form {...layout} name="buyCryptoForm" onFinish={onFinish}>
+              <Form.Item label="Amount" name="amount">
+                <Input type="number" placeholder="Enter amount" />
+              </Form.Item>
+              <Form.Item label="Price" name="price">
+                <Input type="number" placeholder="Enter price" />
+              </Form.Item>
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                  Buy
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
-}
-
-export default CryptoDetails
+          };
+export default CryptoDetails;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Form, Input, Button, Row, Col, Typography, Progress, Card } from 'antd';
+import zxcvbn from 'zxcvbn';
 
 function Login() {
+  const [passwordStrength, setPasswordStrength] = useState(0);
+
+  const onFinish = (values) => {
+    console.log('Login form values:', values);
+  };
+
+  const onFinishRegistration = (values) => {
+    console.log('Registration form values:', values);
+  };
+
+  const handlePasswordChange = (e) => {
+    const password = e.target.value;
+    const result = zxcvbn(password);
+    setPasswordStrength(result.score);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <Typography.Title level={2}>Login</Typography.Title>
+      <Row gutter={[16, 16]}>
+        <Col span={12}>
+          <Card title="Login">
+            <Form
+              name="login"
+              onFinish={onFinish}
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title="Registration">
+            <Form
+              name="registration"
+              onFinish={onFinishRegistration}
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+            >
+              <Form.Item
+                label="Username"
+                name="regUsername"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="regPassword"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+              >
+                <Input.Password onChange={handlePasswordChange} />
+              </Form.Item>
+
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  { required: true, message: 'Please input your email!' },
+                  { type: 'email', message: 'Please enter a valid email address' },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="API Key"
+                name="apiKey"
+                rules={[{ required: true, message: 'Please input your API Key!' }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{ span: 24 }}
+                label="Password Strength"
+              >
+                <Progress
+                  percent={passwordStrength * 25}
+                  status={passwordStrength === 4 ? 'success' : 'active'}
+                />
+              </Form.Item>
+
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                  Register
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
