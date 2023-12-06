3ace2f4871b58cd92bfed1927bc6e18ee15e5032
pawelo472 <pawel.kluczkiewicz@wp.pl>
2023-12-06 14:03:19 +0100

    Wallet działa

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect} from 'react';
-import { Form, Input, Button, Row, Col, Typography, Progress, Card , notification} from 'antd';
+import { Form, Input, Button, Row, Col, Typography, Progress, Card , notification,Space, Table} from 'antd';
 import axios from "axios";
 import { useGetWalletQuery, useGetServerTimeQuery} from '../services/CryptoApi';
+import { Link } from 'react-router-dom';
+import '../App.css';
 const CryptoJS = require('crypto-js');
 
 
  
   function Wallet() {
-    const { data:godzina} = useGetServerTimeQuery();
+
+  const { Title } = Typography;
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [walletData, setWalletData] = useState([]);
+
+    //const { data:godzina} = useGetServerTimeQuery();
   const time_server=Date.now();
     const apiPublic='q1pQetgeBRA3WDaaWVOQM68oYTDWB8mMO9ATg8Lpp74r91KOCHozNeU0RxecULLz';
     const apiSecret= 'cjvelBcWP05hZw6e5UttMzKGqrspYLphfx6Q7LZv0up3sE9QyTjgUk6fvwqcjo36';
@@ -37,7 +44,8 @@ const CryptoJS = require('crypto-js');
             
             }).then(
             (response) => {
-              console.log(response.data);
+              //console.log(response.data);
+              setWalletData(response.data);
           // history.push('/');
           // window.location.reload();
             }).catch(
@@ -51,26 +59,73 @@ const CryptoJS = require('crypto-js');
 //   console.log(wallet);
   
     useEffect(() => {
-        WalletUser();
-        
-
+      const delay = 2000; // Opóźnienie w milisekundach (1 sekunda w tym przypadku)
     
+      const timeoutId = setTimeout(() => {
+        WalletUser();
+      }, delay);
+  
+      // Wyczyszczenie timera przy odmontowaniu komponentu
+      return () => clearTimeout(timeoutId);
       }, []); 
   
+      const filteredCryptos = walletData.filter((crypto) =>
+      crypto.coin.toLowerCase().includes(searchKeyword.toLowerCase())
+    );
 
+    const columns = [
+      {
+        title: 'Coin',
+        dataIndex: 'coin',
+        key: 'coin',
+        //render: (text, record) => <Link to={`/crypto/${record.coin}USDT`}>{text}</Link>,
+      },
+      {
+        title: 'Deposit All Enable',
+        dataIndex: 'depositAllEnable',
+        key: 'depositAllEnable',
+        render: (text) => text.toString(),
+      },
+      {
+        title: 'Free',
+        dataIndex: 'free',
+        key: 'free',
+        sorter: (a, b) => a.free - b.free, // Add this line for sorting
+        //sortOrder: 'descend', // Add this line for default descending order
+      },
+      {
+        title: 'Freeze',
+        dataIndex: 'freeze',
+        key: 'freeze',
+        sorter: (a, b) => a.freeze - b.freeze,
+      },
+      {
+        title: 'Ipoable',
+        dataIndex: 'ipoable',
+        key: 'ipoable',
+      },
+    ];
+  
+
+      
   return(
-    
-    <div>
-      <Typography.Title level={2}>Wallet</Typography.Title>
-      <Row gutter={[16, 16]}>
-        <Col span={12}>
-          <Card title="Wallet">
-        
-            
-          </Card>
-        </Col>
-      </Row>
-    </div>
+    <>
+      <div>
+        <Title level={2}>Wallet</Title>
+        <Space style={{ marginBottom: '16px' }}>
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchKeyword(e.target.value)}
+          />
+        </Space>
+        <Table
+          dataSource={filteredCryptos}
+          columns={columns}
+          rowKey={(record) => record.coin}
+        />
+      </div>
+    </>
   );
+  
  }
 export default Wallet;
